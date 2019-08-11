ae71bf1d23da943d16b5b8c41da4b25e4c8fbb26
Darahasini <41374813+Darahasinisam@users.noreply.github.com>
2019-08-11 14:15:47 +0530

    Update Coffe.java

--- Coffe.java
+++ Coffe.java
@@ -12,6 +12,8 @@ class Coffe
 		//double X = sc.nextDouble();
 		//double P =sc.nextDouble();
 		double sum=0.0;
+		if(X>= 0 && X<=10000 && P >=1 && P<=100)
+		{
 		while(X>1.0)
 		{
 
@@ -22,5 +24,8 @@ class Coffe
 		}
 		sum =sum+1.0;
 		System.out.println((int)sum);
+		}
+		else
+			System.out.println("invalid inputs");
 	}
 }
