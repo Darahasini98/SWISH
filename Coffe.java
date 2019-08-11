91736bc07a3bb0b24b182f0d4ed0464f6cf83e7a
Darahasini <41374813+Darahasinisam@users.noreply.github.com>
2019-08-11 14:09:49 +0530

    Add files via upload

---
+++ Coffe.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Coffe 
+{
+	public static void main(String[] args) 
+	{
+		//System.out.println("Hello World!");
+		Scanner sc= new Scanner(System.in);
+		int Y =sc.nextInt();
+		double X = (double)Y;
+		int Z = sc.nextInt();
+		double P = (double)Z;
+		//double X = sc.nextDouble();
+		//double P =sc.nextDouble();
+		double sum=0.0;
+		while(X>1.0)
+		{
+
+
+			double D = (P*X)/100;
+			sum= sum+ X;
+			X=X-D;
+		}
+		sum =sum+1.0;
+		System.out.println((int)sum);
+	}
+}
